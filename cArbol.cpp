06709b8b33764ffbc7f965ca9ba0fe6b5a5e7926
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2022-03-30 10:44:18 -0600

    Add files via upload

--- cArbol.cpp
+++ cArbol.cpp
@@ -50,12 +50,23 @@ void cArbol::imprimirArbol(cArbol* root) {
 int main() {
 	cArbol* root = NULL;
 	cArbol* obj = new cArbol();
+	int valor, size;
+	cout << "Cuantos valores quieres en tu lista ?"<<endl;
+	cout << ": ";
+	cin >> size;
+	for (int i = 0; i < size; i++) {
+		cout << "Dame un valor para ingresar" << endl;
+		cin >> valor;
+		root = obj->crearArbol(root, valor);
+	}/*
 	root = obj->crearArbol(root, 900);
 	root = obj->crearArbol(root, 300);
 	root = obj->crearArbol(root, 500);
 	root = obj->crearArbol(root, 1200);
 	root = obj->crearArbol(root, 956);
-	root = obj->crearArbol(root, 45);
+	root = obj->crearArbol(root, 45);*/
 	obj->imprimirArbol(root);
 	return 0;
 }
+
+// duinamico cuantos numeros quieres en tu lista y dame los valores para el lunes para el viernes 
